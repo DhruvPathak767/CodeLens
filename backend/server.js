31f1b5d4e31109256b558bfb3e98c354a5429ebc
DhruvPathak767 <dhruvapathak767@gmail.com>
2026-05-25 18:51:34 +0530

    fix railway deployment

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,26 @@ require("dotenv").config();
 
 const app = require("./src/app");
 const connectDB = require("./src/config/db");
+const express = require("express");
+
+app.use(express.json());
 
 // Handle Uncaught Exceptions (Synchronous errors during boot)
 process.on("uncaughtException", (err) => {
   console.error(`[CRITICAL CRASH] Uncaught Exception: ${err.message}`);
   console.error(err.stack);
   process.exit(1);
+});
+const path = require("path");
+
+app.use(express.static(
+  path.join(__dirname, "../frontend/dist")
+));
+
+app.get(/.*/, (req, res) => {
+  res.sendFile(
+    path.join(__dirname, "../frontend/dist/index.html")
+  );
 });
 
 // Establish MongoDB Connection
@@ -18,7 +32,7 @@ connectDB();
 const PORT = process.env.PORT || 5000;
 
 // Start Express Server
-const server = serverListener = app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`=================================================`);
   console.log(`[Server] AI Code Reviewer SaaS Server Online!`);
   console.log(`[Server] Running in Mode: ${process.env.NODE_ENV || "development"}`);
